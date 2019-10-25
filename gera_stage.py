bcbe066b93328c284f09c006cf174a4b92a2db60
denilson <denilson.g.d@hotmail.com>
2019-10-25 14:47:10 -0700

    Reescrito classe de criação de stage

--- gera_stage.py
+++ gera_stage.py
@@ -1,6 +1,20 @@
 import csv_class
 import json_class
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 local = '/home/denilson/Downloads/pagamentos.csv'
 # url_csv = 'https://doc-08-3o-docs.googleusercontent.com/docs/securesc/itgof2bk0rq38g93e3k3bbkt36b80r5j/bukdvqqreor3faj6mco62fgk5cbu3mcn/1571716800000/04105005953058485704/02359347683867043271/1GlYrv7ex0ClxQwQ0NvJ4GTUGre7s8vtw?e=download'
 datas = csv_class.csv_etl.import_csv(local)
@@ -12,7 +26,6 @@ listadados = csv_class.csv_etl.data_treatment(datas)
 url = 'https://demo4417994.mockable.io/clientes/'
 listajson = json_class.json_etl.get_online_json(url)
 list_data_treat = []
-contador_cliente = 0
 for r_csv in listadados:
     for r_json in listajson:
         if r_csv["id_cliente"] == r_json["id"]:
