cbc175c081ff46a0bb7a9cddfe0274f87dce9ea8
denilson <denilson.g.d@hotmail.com>
2019-10-26 11:22:20 -0700

    Refatorado codigo

--- csv_class.py
+++ csv_class.py
@@ -4,83 +4,84 @@ from dateutil.relativedelta import relativedelta
 from datetime import datetime, timedelta, date
 
 
-class csv_etl:
-    # funcao responsavel pela leitura do arquivo csv
-    def import_csv(url_local):
-        print('Inicializando Importação')
-        data = pd.read_csv(url_local, sep=",")
-        print('Finalizado Importação')
-        return data
+# funcao responsavel pela leitura do arquivo csv
+def import_csv(url_local):
+    print('Inicializando Importação')
+    data = pd.read_csv(url_local, sep=",")
+    print('Finalizado Importação')
+    return data
 
-    # funcao responsavel pelo tratamentos dos dados do arquivo csv
-    def data_treatment(csv_data):
-        print('Iniciando tratamento de dados CSV')
-        treat_data = {}
-        list_data_treat = []
 
-        for line in csv_data.values:
-            # Responsavel pelo tratamento do nome do plano
-            fullplano = line[3]
-            plano = fullplano[:len(fullplano) - 2]
+# funcao responsavel pelo tratamentos dos dados do arquivo csv
+def data_treatment(csv_data):
+    print('Iniciando tratamento de dados CSV')
+    treat_data = {}
+    list_data_treat = []
 
-            # Busca os dados de quantos meses foram pagos
-            mes = int(fullplano[len(fullplano) - 1:])
+    for line in csv_data.values:
+        # Responsavel pelo tratamento do nome do plano
+        fullplano = line[3]
+        plano = fullplano[:len(fullplano) - 2]
 
-            # Calculo de valor do produto
-            valor = line[2].replace(',', '.').replace(' ', '')
-            val_convertido = valor[2:].strip()
-            val = float(val_convertido) / mes
+        # Busca os dados de quantos meses foram pagos
+        mes = int(fullplano[len(fullplano) - 1:])
 
-            # Calculo de data mes final do pagamento
-            date_str = line[1]
-            data_final = datetime.strptime(date_str, '%d/%m/%Y').date()
-            data_padrao = datetime.strptime(date_str, '%d/%m/%Y').date()
-            data_padrao = (data_padrao + relativedelta(months=mes - 1))
+        # Calculo de valor do produto
+        valor = line[2].replace(',', '.').replace(' ', '')
+        val_convertido = valor[2:].strip()
+        val = float(val_convertido) / mes
 
-            # Criação de uma lista com dicionarios de todos os pagamentos
-            if mes > 1:
-                for m in range(mes):
-                    data_calculada = (data_final + relativedelta(months=int(m)))
+        # Calculo de data mes final do pagamento
+        date_str = line[1]
+        data_final = datetime.strptime(date_str, '%d/%m/%Y').date()
+        data_padrao = datetime.strptime(date_str, '%d/%m/%Y').date()
+        data_padrao = (data_padrao + relativedelta(months=mes - 1))
 
-                    treat_data = {
-                        'id_cliente': line[0],
-                        'data': data_calculada,
-                        'valor_total': val_convertido,
-                        'plano': plano,
-                        'meses': 1,
-                        'valor_mes': val,
-                        'data_final_pag': data_padrao
-                    }
-                    list_data_treat.append(treat_data)
-            else:
+        # Criação de uma lista com dicionarios de todos os pagamentos
+        if mes > 1:
+            for m in range(mes):
+                data_calculada = (data_final + relativedelta(months=int(m)))
 
                 treat_data = {
                     'id_cliente': line[0],
-                    'data': data_final,
+                    'data': data_calculada,
                     'valor_total': val_convertido,
                     'plano': plano,
-                    'meses': mes,
+                    'meses': 1,
                     'valor_mes': val,
                     'data_final_pag': data_padrao
                 }
-
                 list_data_treat.append(treat_data)
+        else:
 
-        print('Finalizado tratamento CSV')
-        return list_data_treat
+            treat_data = {
+                'id_cliente': line[0],
+                'data': data_final,
+                'valor_total': val_convertido,
+                'plano': plano,
+                'meses': mes,
+                'valor_mes': val,
+                'data_final_pag': data_padrao
+            }
 
-    def write_csv(lista_dict_origem, caminho_csv_destino):
-        print('Iniciado gravação do CSV')
-        ## Obtem as keys da lista passada por parametro
-        colunas = lista_dict_origem[0].keys()
-        array_colunas = []
-        # Cria um array com todas as colunas
-        for r in colunas:
-            array_colunas.append(r)
-        # Efetua a criação do dataframe com base nas colunas obtidas
-        df = DataFrame(lista_dict_origem, columns=array_colunas)
-        # Classifica os dados do CSV nas colunas 0 e 1
-        df = df.sort_values(by=[array_colunas[0], array_colunas[1]])
-        # Cria o CSV
-        export_csv = df.to_csv(caminho_csv_destino, index=None, header=True)
-        print('Processo de scrita finalizado')
+            list_data_treat.append(treat_data)
+
+    print('Finalizado tratamento CSV')
+    return list_data_treat
+
+
+def write_csv(lista_dict_origem, caminho_csv_destino):
+    print('Iniciado gravação do CSV')
+    # Obtem as keys da lista passada por parametro
+    colunas = lista_dict_origem[0].keys()
+    array_colunas = []
+    # Cria um array com todas as colunas
+    for r in colunas:
+        array_colunas.append(r)
+    # Efetua a criação do dataframe com base nas colunas obtidas
+    df = DataFrame(lista_dict_origem, columns=array_colunas)
+    # Classifica os dados do CSV nas colunas 0 e 1
+    df = df.sort_values(by=[array_colunas[0], array_colunas[1]])
+    # Cria o CSV
+    export_csv = df.to_csv(caminho_csv_destino, index=None, header=True)
+    print('Processo de scrita finalizado')
--- json_csv_class.py
+++ json_csv_class.py
@@ -2,32 +2,31 @@ import csv_class
 import json_class
 
 
-class json_csv:
-    def gera_csv_stage(lista_dados_json, lista_dados_csv):
-        # Cria uma lista para armazenar os dados processados
-        list_data_treat = []
-        # Percorre a lista do csv
-        for r_csv in lista_dados_csv:
-            # Percorre a lista do json
-            for r_json in lista_dados_json:
-                # Efetua o join entre as duas listas
-                if r_csv["id_cliente"] == r_json["id"]:
-                    treat_data = {}
-                    # if r_csv.get("id_cliente") == 1030:
-                    # print(f' atual : {r_csv.get("valor_mes")} -> next : {r_csv.get("valor_mes").__next__()}')
-                    #    print(r_csv.__getitem__("valor_mes"))
-                    treat_data = {
-                        'id_cliente': r_csv.get("id_cliente"),
-                        'data_pagamento': r_csv.get("data"),
-                        'valor_total': r_csv.get("valor_total"),
-                        'plano': r_csv.get("plano"),
-                        'meses': r_csv.get("meses"),
-                        'valor_mes': r_csv.get("valor_mes"),
-                        'empresa_nome': r_json.get("nome"),
-                        'cidade': r_json.get("cidade"),
-                        'estado': r_json.get("estado"),
-                        'segmento': r_json.get("segmento"),
-                        'data_pagamento_fim': r_csv.get("data_final_pag")
-                    }
-                    list_data_treat.append(treat_data)
-        return list_data_treat
+def gera_csv_stage(lista_dados_json, lista_dados_csv):
+    # Cria uma lista para armazenar os dados processados
+    list_data_treat = []
+    # Percorre a lista do csv
+    for r_csv in lista_dados_csv:
+        # Percorre a lista do json
+        for r_json in lista_dados_json:
+            # Efetua o join entre as duas listas
+            if r_csv["id_cliente"] == r_json["id"]:
+                treat_data = {}
+                # if r_csv.get("id_cliente") == 1030:
+                # print(f' atual : {r_csv.get("valor_mes")} -> next : {r_csv.get("valor_mes").__next__()}')
+                #    print(r_csv.__getitem__("valor_mes"))
+                treat_data = {
+                    'id_cliente': r_csv.get("id_cliente"),
+                    'data_pagamento': r_csv.get("data"),
+                    'valor_total': r_csv.get("valor_total"),
+                    'plano': r_csv.get("plano"),
+                    'meses': r_csv.get("meses"),
+                    'valor_mes': r_csv.get("valor_mes"),
+                    'empresa_nome': r_json.get("nome"),
+                    'cidade': r_json.get("cidade"),
+                    'estado': r_json.get("estado"),
+                    'segmento': r_json.get("segmento"),
+                    'data_pagamento_fim': r_csv.get("data_final_pag")
+                }
+                list_data_treat.append(treat_data)
+    return list_data_treat
