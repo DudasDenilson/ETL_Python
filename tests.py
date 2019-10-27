dccabce5b9a41ceb0c88608b9c78195028b0ed45
denilson <denilson.g.d@hotmail.com>
2019-10-26 22:39:56 -0700

    Reescrito testes JSON

--- tests.py
+++ tests.py
@@ -6,59 +6,38 @@ import tests_data
 
 class JSONTreatment(unittest.TestCase):
     def test_get_json_with_invalid_url(self):
-        url = tests_data.URL_JSON_INVALID
-        result = get_online_json(url)
-
+        result = get_online_json(tests_data.URL_JSON_INVALID)
         self.assertEqual(result, None)
 
     def test_get_json_with_valid_url(self):
-        url = tests_data.URL_JSON_VALID
-        result = get_online_json(url)
-
+        result = get_online_json(tests_data.URL_JSON_VALID)
         self.assertNotEqual(result, None)
 
     def test_get_json_local(self):
-        url = tests_data.LOCAL_VALID_JSON
-        result = get_local_json(url)
-
+        result = get_local_json(tests_data.LOCAL_VALID_JSON)
         self.assertNotEqual(result, None)
 
     def test_get_json_local_invalid(self):
-        url = tests_data.LOCAL_INVALID_JSON
-        result = get_local_json(url)
-
+        result = get_local_json(tests_data.LOCAL_INVALID_JSON)
         self.assertEqual(result, None)
 
     def test_treat_json_valid(self):
-        list_json = tests_data.LISTA_JSON_FULL
-
-        url = tests_data.URL_JSON_VALID
-        result = treat_json(get_online_json(url))
-
-        self.assertListEqual(result, list_json)
+        result = treat_json(get_online_json(tests_data.URL_JSON_VALID))
+        self.assertListEqual(result, tests_data.LISTA_JSON_FULL)
 
     def test_treat_json_invalid(self):
-        list_json = tests_data.LISTA_JSON_FULL
-        url = tests_data.LOCAL_VALID_JSON
-        result = treat_json(get_local_json(url))
-
-        self.assertNotEqual(result, list_json)
+        result = treat_json(get_local_json(tests_data.LOCAL_VALID_JSON))
+        self.assertNotEqual(result, tests_data.LISTA_JSON_FULL)
 
 
 class CSVTreatment(unittest.TestCase):
     def test_get_csv_with_invalid_url(self):
-        data = None
-        local = tests_data.LOCAL_INVALID_CSV
-        result = import_csv(local)
-
-        self.assertIsNone(result, data)
+        result = import_csv(tests_data.LOCAL_INVALID_CSV)
+        self.assertIsNone(result)
 
     def test_get_csv_with_valid_url(self):
-        data = None
-        local = tests_data.LOCAL_VALID_PART_CSV
-        result = import_csv(local)
-
-        self.assertIsNotNone(result, data)
+        result = import_csv(tests_data.LOCAL_VALID_PART_CSV)
+        self.assertIsNotNone(result)
 
 
 
